7834876278f4d5438bf2a78aecfa0dd0b02921a7
StevenKampinga <Steven.Kampinga@Outlook.com>
2016-12-22 13:39:26 +0100

    Multiplayer Done

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : NetworkBehaviour {
+    private float moveSpeed = 10f;
+    private Vector3 inputValue;
+ 
+	void Update () {
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        inputValue.y = 0f;
+        inputValue.z = CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime *  moveSpeed;
+
+        transform.Translate(inputValue);
+    }
+
+    public override void OnStartLocalPlayer () {
+        GetComponentInChildren<Camera>().enabled = true;
+    }
+}
