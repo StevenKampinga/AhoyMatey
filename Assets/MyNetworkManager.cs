912830b6f2464893da034303606ff81fb5b4f202
StevenKampinga <Steven.Kampinga@Outlook.com>
2016-12-22 10:14:37 +0100

    NetworkManager Overrides

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost () {
+        Debug.Log(Time.timeSinceLevelLoad + ": Starting Host...");
+        StartHost();
+    }
+
+    public override void OnStartHost () {
+        Debug.Log(Time.timeSinceLevelLoad + ": Host Started!");
+    }
+
+    public override void OnStartClient(NetworkClient myClient) {
+        Debug.Log(Time.timeSinceLevelLoad + ": Client Start Requested...");
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn) {
+        Debug.Log(Time.timeSinceLevelLoad + ": Client Connected!");
+        Debug.Log(Time.timeSinceLevelLoad + ": Client Address: " + conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots () {
+        Debug.Log(".");
+    }
+}
